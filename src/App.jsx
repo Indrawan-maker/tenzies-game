2dff33e4556a3777998ca3cc8449af3b746b979a
Indrawan <indrawanismail0@gmail.com>
2025-07-10 21:49:27 +0800

    Finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,77 @@
-import Die from "./Die"
 import { useState } from "react"
-import { nanoid } from 'nanoid'
+import Die from "./Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+import  { useRef, useEffect } from "react"
+
+
 
 export default function App() {
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(() => generateAllNewDice())
+    
+    const btnRef = useRef(null)
+    
+    useEffect(() => {
+        if(gameWon) {btnRef.current.focus()} 
+    }, [btnRef])
+    
+
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
 
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
-            .map(() => 
-            ({
-                value : Math.ceil(Math.random() * 6),
-                isHold: false,
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
                 id: nanoid()
-            })
-            )
+            }))
     }
-
+    
     function rollDice() {
-        setDice(prevDice => prevDice.map(prevDiceObj => 
-            prevDiceObj.isHold == false ? {
-                ...prevDiceObj,
-                value: Math.ceil(Math.random() * 6)
-            } : prevDiceObj
-        ))
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        } else {
+            setDice(generateAllNewDice())
+        }
     }
 
     function hold(id) {
-        setDice(prevDice => prevDice.map(prevDiceObj => 
-            prevDiceObj.id == id ? 
-            {
-                ...prevDiceObj,
-                isHold: !prevDiceObj.isHold
-            } : prevDiceObj
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
         ))
-        console.log(dice)
     }
 
-    const newDice = dice.map(diceObj => 
-        <Die 
-        key={diceObj.id} 
-        value={diceObj.value} 
-        isHold={() => hold(diceObj.id)} 
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
         />
-    )
-
+    ))
 
     return (
         <main>
-            <div className="dice-container">
-                {newDice}
+            {gameWon && <Confetti />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
             </div>
-                <div>
-                <button className="roll-dice" onClick={rollDice}>Roll</button>
-                </div>
-
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice} ref={btnRef}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,13 +1,15 @@
 export default function Die(props) {
-    const myStyle = {
-        backgroundColor : props.isHold ? "red" : "white"
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+    
     return (
         <button 
-        style={myStyle}
-        onClick={props.isHold}
-        >
-            {props.value}
-            </button>
+            style={styles}
+            onClick={props.hold}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+        >{props.value}</button>
     )
 }
